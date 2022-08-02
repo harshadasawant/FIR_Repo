e6cdff26646b894bb9bf13a47d9c83f97312c444
harshada <harshadasawant22@gmail.com>
2022-08-03 03:33:37 +0530

    added import

--- src/main/java/com/police/fir/repository/FirDetailRepository.java
+++ src/main/java/com/police/fir/repository/FirDetailRepository.java
@@ -1,6 +1,7 @@
 package com.police.fir.repository;
 
 import com.police.fir.entity.FirDetail;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
